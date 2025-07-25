8e11758e64d3bb63bb5b92fd5c284a7f8e2ee3f9
AHpx <ahpx@yandex.com>
2025-07-25 21:19:31 +0800

    feat: add advanced input

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,12 +1,20 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { AdvancedInput } from "@/components/ui/advanced-input";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function TestPage() {
   return (
-    <div className="flex flex-col bg-[#FFF] justify-center h-full container mx-auto p-4 space-y-8">
-      <Input className='h-15' placeholder="这个是input" />
-      <Button className='h-15'>下一步</Button>
-      <Button variant={'outline'} className='h-15'>下一步</Button>
+    <div className="flex flex-col bg-[#FFF] justify-center h-full container mx-auto p-4 space-y-16">
+      <Input className="h-15" placeholder="这个是input" />
+      <AdvancedInput className="h-15" popoverContent={<div>Your custom content here</div>}>
+        Click me
+      </AdvancedInput>
+      <Button className="h-15">下一步</Button>
+      
+      <Button variant={"outline"} className="h-15">
+        下一步
+      </Button>
+      
     </div>
   );
 }
