b94ac273a650575330e15f91cb9f1c65d1c5e05d
AHpx <ahpx@yandex.com>
2025-07-26 17:44:23 +0800

    ui: add jump logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
-  return (
-    <div>
+"use client";
 
+import { useEffect } from 'react';
+import Image from "next/image";
+import { useRouter } from 'next/navigation';
+import { useProfileStore } from '@/stores/profileStore';
+
+export default function Home() {
+  const router = useRouter();
+  const { basicInfo } = useProfileStore();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (basicInfo.name) {
+        router.push('/profile');
+      } else {
+        router.push('/basic-info');
+      }
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [basicInfo.name, router]);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Image src="/logo.svg" alt="logo" width={184} height={184} />
     </div>
   );
 }
